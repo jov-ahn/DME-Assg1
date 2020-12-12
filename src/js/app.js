4caddf9047ff0abe987c2d1eb81ef19d1ee55483
Misty <jov4hn@gmail.com>
2020-12-12 19:31:12 +0800

    Version 1.0
    [+] App Layout
    [+] Edit feature

---
+++ src/js/app.js
@@ -0,0 +1,72 @@
+let stories = [];
+
+class Story {
+    constructor(title, contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
+
+function loadStories() {
+    for (let i = 0; i < stories.length; i++) {
+        $('#list-of-stories').prepend(`
+            <ion-item-sliding class="view-story-details" id=${i}>
+                <ion-item>
+                    <ion-label>${stories[i].title}</ion-label>
+                </ion-item>
+
+                <ion-item-options side="start">
+                    <ion-item-option color="primary" class="edit-story-btn">Edit</ion-item-option>
+                </ion-item-options>
+
+                <ion-item-options side="end">
+                    <ion-item-option color="danger" class="delete-story-btn">Delete</ion-item-option>
+                </ion-item-options>
+            </ion-item-sliding>
+        `);
+    }
+}
+
+async function presentToast(msg, color) {
+    const toast = document.createElement('ion-toast');
+    toast.message = msg;
+    toast.color = color;
+    toast.duration = 2000;
+
+    document.body.appendChild(toast);
+    return toast.present();
+}
+
+$(document).ready(function () {
+    if (localStorage.getItem('stories') === null) {
+        let defaultStory = new Story('Welcome to Storybook!', 'To get started, tap on the plus icon near the bottom of your screen.');
+        
+        stories.push(defaultStory);
+        localStorage.setItem('stories', JSON.stringify(stories));
+    }
+    else {
+        stories = JSON.parse(localStorage.getItem('stories'));
+    }
+
+    loadStories();
+
+    $(document).on('click', '#add-story', function () {
+        if ($('#story-title').val() !== "" && $('#story-contents').val() !== "") {
+            const storyTitle = $('#story-title').val();
+            const storyContents = $('#story-contents').val();
+            const newStory = new Story(storyTitle, storyContents);
+            
+            stories.push(newStory);
+            localStorage.setItem('stories', JSON.stringify(stories));
+            presentToast(`New Story Added: ${storyTitle}`, 'success');
+        }
+        else {
+            presentToast('Error: Missing fields', 'danger');
+        }
+    });
+
+    $(document).on('click', '.edit-story-btn', function() {
+        sessionStorage.setItem('storyToEdit', JSON.stringify($(this).closest('.view-story-details').attr('id')));
+        window.location.href = 'edit.html';
+    })
+});
---
+++ src/js/edit.js
@@ -0,0 +1,39 @@
+async function presentToast(msg, color) {
+    const toast = document.createElement('ion-toast');
+    toast.message = msg;
+    toast.color = color;
+    toast.duration = 2000;
+
+    document.body.appendChild(toast);
+    return toast.present();
+}
+
+$(document).ready(function() {
+    const storyToEdit = JSON.parse(sessionStorage.getItem('storyToEdit'));
+
+    let stories = JSON.parse(localStorage.getItem('stories'));
+    let storyTitle = stories[storyToEdit].title;
+    let storyContents = stories[storyToEdit].contents;
+
+    $('#edit-story-title').val(storyTitle);
+    $('#edit-story-contents').val(storyContents);
+
+    $('#save-story-changes-btn').click(function () {
+        if ($('#edit-story-title').val() !== "" && $('#edit-story-contents').val() !== "") {
+            const newStoryTitle = $('#edit-story-title').val();
+            const newStoryContents = $('#edit-story-contents').val();
+            
+            stories[storyToEdit].title = newStoryTitle;
+            stories[storyToEdit].contents = newStoryContents;
+            
+            localStorage.setItem('stories', JSON.stringify(stories));
+            presentToast(`Story renamed to ${newStoryTitle}`, 'success');
+            setTimeout(function () {
+                window.location.href = 'index.html';
+            }, 1000);
+        }
+        else {
+            presentToast('Error: Missing fields', 'danger');
+        }
+    });
+});
